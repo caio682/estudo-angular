fe63626a53bd7954730f620845fceac14ba0890c
caio682 <79763392+caio682@users.noreply.github.com>
2023-05-28 12:37:21 -0300

    criando o card news

--- src/components/TituloDinamico.js
+++ src/components/TituloDinamico.js
@@ -7,15 +7,14 @@ class TituloDinamico extends HTMLElement {
 
         //base do component 
         const componentRoot = document.createElement("h1");
-        componentRoot.textContent = this.getAttribute("titulo")
-        // componentRoot.textContent = 'Felipe'
+        componentRoot.textContent = this.getAttribute("titulo");
         
         //estilizar o componet
-        const style = document.createElement('style')
+        const style = document.createElement("style");
         style.textContent = `
         h1{
             color: red;
-        }`
+        }`;
 
         //enviar para a shadow
         shadow.appendChild(componentRoot);
