43559dcc03c74bb22a6246cb4d9bef5b14a242db
caio682 <79763392+caio682@users.noreply.github.com>
2023-05-29 18:20:51 -0300

    componente criado do zero cardnews

--- src/components/CardNews.js
+++ src/components/CardNews.js
@@ -1,31 +1,86 @@
-class Cardnews extends HTMLElement{
-    constructor(){
+class Cardnews extends HTMLElement {
+    constructor() {
         super();
 
-        const shadow = this.attachShadow({mode: open});
+        const shadow = this.attachShadow({ mode: "open" });
         shadow.appendChild(this.build());
-        shadow.appendChild(this.style());
+        shadow.appendChild(this.styles());
     }
 
     build() {
         const componentRoot = document.createElement("div");
         componentRoot.setAttribute("class", "card");
 
-        
         const cardLeft = document.createElement("div");
-        const cardRight = document.createElement("div");
+        cardLeft.setAttribute("class", "card__left");
 
+        const autor = document.createElement("span");
+        autor.textContent = "by " + (this.getAttribute("autor") || "Anonymous");
+
+        const linkTitle = document.createElement("a");
+        linkTitle.textContent = this.getAttribute("title")
+        linkTitle.href = this.getAttribute("link-url")
+
+        const newsContent = document.createElement("p");
+        newsContent.textContent = this.getAttribute("content")
+
+        cardLeft.appendChild(autor);
+        cardLeft.appendChild(linkTitle);
+        cardLeft.appendChild(newsContent);
+
+        const cardRight = document.createElement("div");
+        cardRight.setAttribute("class", "card__right");
+
+        const newsImage = document.createElement("img");
+        newsImage.src = this.getAttribute("photo") || "assets/foto-default.png"
+        newsImage.alt = "Foto da Notícia"
+        cardRight.appendChild(newsImage);
 
 
         componentRoot.appendChild(cardLeft);
         componentRoot.appendChild(cardRight);
 
-
         return componentRoot;
     }
-    styles() {}
+
+    styles() {
+        const style = document.createElement("style");
+        style.textContent = `
+        @font-face {
+            font-family: myFirstFont;
+            src: url(starjedi/Starjedi.ttf);
+          }
+       
+      .card {
+        wight = 80%;
+        box-shadow: 9px 9px 27px 0px rgba(0, 0, 0, 0.75);
+        -webkit-box-shadow: 9px 9px 27px 0px rgba(0, 0, 0, 0.75);
+        -moz-box-shadow: 9px 9px 27px 0px rgba(0, 0, 0, 0.75);
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+
+      }
+      .card__left {
+        display: flex;
+        flex-direction: column;
+        justify-content:center;
+        padding-left:10px;
+      }
+      .card__left > a {
+        margin-top: 15px;
+        font-size: 25px;
+        color: black;
+        text-decoration:none;
+        font-weight:bold;
+        
+      }
+      .card__lef > p {
+        color: rgb(70, 70, 70);
+      }
+      `;
+        return style;
+    }
 }
-
-
 
 customElements.define("card-news", Cardnews);
--- src/components/TituloDinamico.js
+++ src/components/TituloDinamico.js
@@ -2,25 +2,20 @@ class TituloDinamico extends HTMLElement {
     constructor() {
         super();
 
+        const shadow = this.attachShadow({ mode: "open" });
 
-        const shadow = this.attachShadow({mode: "open"});
-
-        //base do component 
         const componentRoot = document.createElement("h1");
         componentRoot.textContent = this.getAttribute("titulo");
-        
-        //estilizar o componet
+
         const style = document.createElement("style");
         style.textContent = `
-        h1{
+        h1 {
             color: red;
         }`;
 
-        //enviar para a shadow
         shadow.appendChild(componentRoot);
         shadow.appendChild(style);
     }
 }
 
-
-customElements.define('titulo-dinamico', TituloDinamico);
+customElements.define("titulo-dinamico", TituloDinamico);
